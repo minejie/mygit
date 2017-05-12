9996cbe7d6f4d5ba77a039f67ec63b4c6b09d530
minejie <caozhengjie823914@163.com>
2017-05-12 20:49:26 +0800

    recyclerviewCheck

---
+++ MyRecyclerviewCheck/app/src/main/java/com/example/admin/myrecyclerviewcheck/MainActivity.java
@@ -0,0 +1,106 @@
+package com.example.admin.myrecyclerviewcheck;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private List<String> list;
+    private ListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        RecyclerView recyclerView=(RecyclerView) findViewById(R.id.recyclerview);
+        findViewById(R.id.commit).setOnClickListener(this);
+        initData();
+        //布局管理器
+        LinearLayoutManager manager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(manager);
+        recyclerView.setHasFixedSize(true);
+        adapter = new ListAdapter(list, this);
+        recyclerView.setAdapter(adapter);
+
+        //添加分割线
+        recyclerView.addItemDecoration(new MyDividerLine(this, MyDividerLine.VERTICAL_LIST));
+        adapter.setRecyclerViewOnItemClickListener(new ListAdapter.RecyclerViewOnItemClickListener() {
+            @Override
+            public void onItemClickListener(View view, int position) {
+                //设置选中的项
+                adapter.setSelectItem(position);
+            }
+
+            @Override
+            public boolean onItemLongClickListener(View view, int position) {
+                adapter.setShowBox();
+                //设置选中的项
+                adapter.setSelectItem(position);
+                adapter.notifyDataSetChanged();
+                return true;
+            }
+        });
+
+    }
+    @Override
+    public void onClick(View v) {
+        Intent it=new Intent(MainActivity.this,Second.class);
+        startActivity(it);
+        //获取你选中的item
+        Map<Integer, Boolean> map = adapter.getMap();
+        for (int i = 0; i < map.size(); i++) {
+            if (map.get(i)) {
+                Log.d("TAG", "你选了第：" + i + "项");
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            //全选
+            case R.id.all:
+                Map<Integer, Boolean> map = adapter.getMap();
+                for (int i = 0; i < map.size(); i++) {
+                    map.put(i, true);
+                    adapter.notifyDataSetChanged();
+                }
+                break;
+            //全不选
+            case R.id.no_all:
+                Map<Integer, Boolean> m = adapter.getMap();
+                for (int i = 0; i < m.size(); i++) {
+                    m.put(i, false);
+                    adapter.notifyDataSetChanged();
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initData() {
+        list = new ArrayList<>();
+        for (int i=1;i<30;i++){
+            list.add("CHINA  IS NO.1! Do You Agree? YES +"+i);
+        }
+
+    }
+
+
+}
---
+++ MyRecyclerviewCheck/app/src/main/java/com/example/admin/myrecyclerviewcheck/Second.java
@@ -0,0 +1,18 @@
+package com.example.admin.myrecyclerviewcheck;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Date：2017/5/12
+ * author: 曹政杰Administrator.
+ * function：
+ */
+
+public class Second extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sec);
+    }
+}
